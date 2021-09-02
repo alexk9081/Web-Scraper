0f0555999399f8b266b9d675e5cb51a53b25e4aa
Alex <Alex@DESKTOP-BVPJRDN.attlocal.net>
2021-09-02 16:02:38 -0400

    Completed web scraper for Google home page image

---
+++ Web Scraping Platform/src/WebScraper.java
@@ -0,0 +1,35 @@
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URL;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
+
+public class WebScraper {
+	public static void main(String[] args) {
+		String source = "http://www.google.com";
+		Document doc = null;
+		Elements pictureClass = null;
+		Elements googlePicture = null;
+		String pictureSource = null;
+		try {
+			doc = Jsoup.connect(source).get();
+			pictureClass = doc.select("div[class=\"k1zIA kKvsb\"]");
+			googlePicture = pictureClass.select("img#hplogo");
+			pictureSource = googlePicture.attr("src");
+		} catch (IOException e) {
+			System.out.print("Connection Error.");
+			e.printStackTrace();
+		}
+		try {
+		    Desktop.getDesktop().browse(new URL(source + pictureSource).toURI());
+		} catch (Exception e) {}
+//		System.out.print(pictureSource);
+	}
+
+}
