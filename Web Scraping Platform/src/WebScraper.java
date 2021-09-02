3b875e1049f1667994308ec078b7a7f2ae60fdac
Alex <Alex@DESKTOP-BVPJRDN.attlocal.net>
2021-09-02 16:03:16 -0400

    Removed unused imports

--- Web Scraping Platform/src/WebScraper.java
+++ Web Scraping Platform/src/WebScraper.java
@@ -3,11 +3,8 @@ import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URL;
 
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
 
 public class WebScraper {
