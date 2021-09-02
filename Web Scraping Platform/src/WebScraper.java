c8e71e4f000ed19f481974b41250a765e0b40c35
Alex <Alex@DESKTOP-BVPJRDN.attlocal.net>
2021-09-02 16:04:46 -0400

    Cleaned code by localizing variables and removing commented line of code

--- Web Scraping Platform/src/WebScraper.java
+++ Web Scraping Platform/src/WebScraper.java
@@ -10,11 +10,13 @@ import org.jsoup.select.Elements;
 public class WebScraper {
 	public static void main(String[] args) {
 		String source = "http://www.google.com";
-		Document doc = null;
-		Elements pictureClass = null;
-		Elements googlePicture = null;
 		String pictureSource = null;
+		
 		try {
+			Document doc = null;
+			Elements pictureClass = null;
+			Elements googlePicture = null;
+			
 			doc = Jsoup.connect(source).get();
 			pictureClass = doc.select("div[class=\"k1zIA kKvsb\"]");
 			googlePicture = pictureClass.select("img#hplogo");
@@ -23,10 +25,10 @@ public class WebScraper {
 			System.out.print("Connection Error.");
 			e.printStackTrace();
 		}
+		
 		try {
 		    Desktop.getDesktop().browse(new URL(source + pictureSource).toURI());
 		} catch (Exception e) {}
-//		System.out.print(pictureSource);
 	}
 
 }
